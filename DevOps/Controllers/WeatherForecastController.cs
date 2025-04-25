77b3fcefd6f0807b0ef101f19b26de556d96b770
ramhaz <160605483+ramhaz@users.noreply.github.com>
2025-04-25 13:09:32 +0200

    aasdasads

--- DevOps/Controllers/WeatherForecastController.cs
+++ DevOps/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DevOps.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Fr", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
